9937ddbbce033b2fd7e6c6b959ab21c0b52a4f35
ganeshdarewar <ganeshdarewar@gmail.com>
2024-02-09 10:18:54 +0530

    week2 assignment done

--- week2/assignment/functions.cpp
+++ week2/assignment/functions.cpp
@@ -30,3 +30,92 @@
 // };
 
 
+// #include <iostream>
+// #include <limits>
+// using namespace std;
+
+// int main(){
+//   int n;
+//   cin>>n;
+//   int ans=0,rem=0;
+//   bool isnegative=false;
+//   if(n<=INT_MIN){
+//     return 0;
+//   }
+//   if(n<0){
+//     isnegative =true;
+//     n=-n;
+//   }
+//   while(n>0){
+//     if(x>INT_MAX/10){
+//       return 0;
+//     }
+//     if(n)
+//     rem=n%10;
+//     ans=ans*10+rem;
+//     n=n/10;
+//   }
+//   if(isnegative){
+//     cout<<-ans;
+//   }
+//   else{
+//     cout<<ans;
+//   }
+//   return 0;
+// }
+
+// https://www.geeksforgeeks.org/problems/set-kth-bit3724/1
+// class Solution
+// {
+// public:
+//     int setKthBit(int N, int K)
+//     {
+//         // Write Your Code here
+//         // int mask=1<<K;
+//         // int ans=N|mask;
+//         // return ans;
+        
+//         return N|(1<<K);
+//     }
+    
+// };
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//   int n;
+//   cin>>n;
+//   int k=1;
+//   cout<<(n | (1<<k));
+//   return 0;
+// }
+
+
+
+// https://leetcode.com/problems/convert-the-temperature/
+// class Solution {
+// public:
+//     vector<double> convertTemperature(double celsius) {
+//        double k = celsius + 273.15;
+//        double f = celsius * 1.80 + 32.00;
+//        vector<double> v;
+//         v.push_back(k);
+//        v.push_back(f);
+     
+//        return v;
+//     }
+// };
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//   double c;
+//   cin>>c;
+//     double k = c + 273.15;
+//     double f = c * 1.80 + 32.00;
+//     cout<<"Kelvin "<<k<<endl;
+//     cout<<"Fahrenheit  "<<f<<endl;
+//   return 0;
+// }
