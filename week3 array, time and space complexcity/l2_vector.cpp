b4bf4938ce0e836e9e484efeb653234383a00344
ganeshdarewar <ganeshdarewar@gmail.com>
2024-05-17 15:07:24 +0530

    week 3 lecture 2 completed

--- week3 array, time and space complexcity/l2_vector.cpp
+++ week3 array, time and space complexcity/l2_vector.cpp
@@ -121,4 +121,107 @@
 
 
 
-// union with avoid duplicates time 1.5
+// union with avoid duplicates 
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int main(){
+//     vector<int> a{1,2,3,4,5};
+//     vector<int> b{6,7,2,8};
+//     vector<int> ans;
+//     for(int i=0; i<a.size();i++){
+//         for(int j=0; j<b.size(); j++){
+//             if(a[i]==b[j]){
+//                 b[j]=INT_MIN;
+//             }
+//         }
+//     }
+//     for(int i=0; i<a.size(); i++){
+//         ans.push_back(a[i]);
+//     }
+//     for(int i=0; i<b.size(); i++){
+//         if(b[i]!=INT_MIN){
+//             ans.push_back(b[i]);
+//         }
+//     }
+//     for(int i=0; i<ans.size();i++){
+//         cout<<ans[i]<<" ";
+//     }
+//     return 0;
+// }
+
+
+// pair sum i.e 3+6 =9
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main(){
+//     vector<int> a{1, 3, 5, 7, 4, 6};
+//     int sum=9;
+//     for(int i = 0; i < a.size(); i++){
+//         for(int j = i + 1; j < a.size(); j++){
+//             if(a[i] + a[j] == sum){
+//                 cout << a[i] << " " << a[j] << endl;   
+//             }
+//     }
+//     }
+//     return 0;
+// }
+
+
+// triplet sums 10,20,30 =50
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main(){
+//     vector<int> a{10,20,30,40,50,60,70,80,90,100};
+//     for(int i=0; i<a.size(); i++){
+//         for(int j=i+1; j<a.size();j++){
+//             for(int k=j+1; k<a.size(); k++){
+//                 if(a[i]+a[j]+a[k]==80){
+//                     cout<<a[i]<<" "<<a[j]<<" "<<a[k]<<endl; 
+//                 }
+//             }
+//         }
+//     }
+//     return 0;
+// }
+
+
+
+
+// sort 0s and 1s
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> a{0,1,1,0,1};
+    int start=0;
+    int end=a.size()-1;
+    while(start<=end){
+        if(a[start]>a[end]){
+            // int temp= a[end];
+            // a[end]=a[start];
+            // a[start]=temp;
+            swap(a[start], a[end]);
+            start++;
+            end--;
+        }
+        else{
+             start++;
+             end--;
+        }
+    }
+    // for(int i=0; i<a.size(); i++){
+    //     cout<<a[i]<<" ";
+    // }
+    for(auto val : a){
+        cout<<val<<" ";
+    }
+    return 0;
+}
